deabbc6a79feace594eb87b2975250ddc0cd00b6
sandreae <git@contact.samandreae.com>
2021-02-03 19:46:05 +0000

    Don't server public files

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ require('dotenv').config()
 const isProduction = process.env.NODE_ENV === 'production'
 const app = express()
 
-app.use(express.static(path.join(__dirname, '../public')));
+// app.use(express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors())
