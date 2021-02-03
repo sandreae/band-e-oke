2da40574874a26d816e06e0ed76e220a6881eb03
sandreae <git@contact.samandreae.com>
2021-02-03 00:45:53 +0000

    Upload new songs

--- src/components/App.js
+++ src/components/App.js
@@ -13,71 +13,46 @@ import Login from "./users/Login";
 import { ToastContainer, cssTransition } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
-import legalIllegalTrack from '../../public/legal-illegal.mp3'
-import legalIllegalEb from '../../public/legal-illegal-Eb.musicxml'
-import legalIllegalBb from '../../public/legal-illegal-Bb.musicxml'
-import legalIllegalC from '../../public/legal-illegal-C.musicxml'
+import legalIllegalTrack from '../../public/scores/legal-illegal/legal-illegal.mp3'
+import legalIllegalEbPdf from '../../public/scores/legal-illegal/legal-illegal-Eb.pdf'
+import legalIllegalBbPdf from '../../public/scores/legal-illegal/legal-illegal-Bb.pdf'
+import legalIllegalCPdf from '../../public/scores/legal-illegal/legal-illegal-C.pdf'
 import iNeedYouTrack from '../../public/scores/i-need-you/i-need-you.mp3'
-import iNeedYouEb from '../../public/scores/i-need-you/i-need-you-Eb.musicxml'
-import iNeedYouBb from '../../public/scores/i-need-you/i-need-you-Bb.musicxml'
-import iNeedYouC from '../../public/scores/i-need-you/i-need-you-C.musicxml'
 import iNeedYouEbPdf from '../../public/scores/i-need-you/i-need-you-Eb.pdf'
 import iNeedYouBbPdf from '../../public/scores/i-need-you/i-need-you-Bb.pdf'
 import iNeedYouCPdf from '../../public/scores/i-need-you/i-need-you-C.pdf'
 import eyeOfTheTigerTrack from '../../public/scores/eye-of-the-tiger/eye-of-the-tiger.mp3'
-import eyeOfTheTigerEb from '../../public/scores/eye-of-the-tiger/eye-of-the-tiger-Eb.musicxml'
-import eyeOfTheTigerBb from '../../public/scores/eye-of-the-tiger/eye-of-the-tiger-Bb.musicxml'
-import eyeOfTheTigerC from '../../public/scores/eye-of-the-tiger/eye-of-the-tiger-C.musicxml'
 import eyeOfTheTigerEbPdf from '../../public/scores/eye-of-the-tiger/eye-of-the-tiger-Eb.pdf'
 import eyeOfTheTigerBbPdf from '../../public/scores/eye-of-the-tiger/eye-of-the-tiger-Bb.pdf'
 import eyeOfTheTigerCPdf from '../../public/scores/eye-of-the-tiger/eye-of-the-tiger-C.pdf'
 import madWorldTrack from '../../public/scores/mad-world/mad-world.mp3'
-import madWorldEb from '../../public/scores/mad-world/mad-world-Eb.musicxml'
-import madWorldBb from '../../public/scores/mad-world/mad-world-Bb.musicxml'
-import madWorldC from '../../public/scores/mad-world/mad-world-C.musicxml'
 import madWorldEbPdf from '../../public/scores/mad-world/mad-world-Eb.pdf'
 import madWorldBbPdf from '../../public/scores/mad-world/mad-world-Bb.pdf'
 import madWorldCPdf from '../../public/scores/mad-world/mad-world-C.pdf'
 import pinkPantherTrack from '../../public/scores/pink-panther/pink-panther.mp3'
-import pinkPantherEb from '../../public/scores/pink-panther/pink-panther-Eb.musicxml'
-import pinkPantherBb from '../../public/scores/pink-panther/pink-panther-Bb.musicxml'
-import pinkPantherC from '../../public/scores/pink-panther/pink-panther-C.musicxml'
 import pinkPantherEbPdf from '../../public/scores/pink-panther/pink-panther-Eb.pdf'
 import pinkPantherBbPdf from '../../public/scores/pink-panther/pink-panther-Bb.pdf'
 import pinkPantherCPdf from '../../public/scores/pink-panther/pink-panther-C.pdf'
 import godsPlanTrack from '../../public/scores/gods-plan/gods-plan.mp3'
-import godsPlanEb from '../../public/scores/gods-plan/gods-plan-Eb.musicxml'
-import godsPlanBb from '../../public/scores/gods-plan/gods-plan-Bb.musicxml'
-import godsPlanC from '../../public/scores/gods-plan/gods-plan-C.musicxml'
 import godsPlanEbPdf from '../../public/scores/gods-plan/gods-plan-Eb.pdf'
 import godsPlanBbPdf from '../../public/scores/gods-plan/gods-plan-Bb.pdf'
 import godsPlanCPdf from '../../public/scores/gods-plan/gods-plan-C.pdf'
 import odeToJoyTrack from '../../public/scores/ode-to-joy/ode-to-joy.mp3'
-import odeToJoyEb from '../../public/scores/ode-to-joy/ode-to-joy-Eb.musicxml'
-import odeToJoyBb from '../../public/scores/ode-to-joy/ode-to-joy-Bb.musicxml'
 import odeToJoyC from '../../public/scores/ode-to-joy/ode-to-joy-C.musicxml'
 import odeToJoyEbPdf from '../../public/scores/ode-to-joy/ode-to-joy-Eb.pdf'
 import odeToJoyBbPdf from '../../public/scores/ode-to-joy/ode-to-joy-Bb.pdf'
 import odeToJoyCPdf from '../../public/scores/ode-to-joy/ode-to-joy-C.pdf'
 import happierTrack from '../../public/scores/happier/happier.mp3'
-import happierEb from '../../public/scores/happier/happier-Eb.musicxml'
-import happierBb from '../../public/scores/happier/happier-Bb.musicxml'
-import happierC from '../../public/scores/happier/happier-C.musicxml'
 import happierEbPdf from '../../public/scores/happier/happier-Eb.pdf'
 import happierBbPdf from '../../public/scores/happier/happier-Bb.pdf'
 import happierCPdf from '../../public/scores/happier/happier-C.pdf'
 import filterTrack from '../../public/scores/filter/filter.mp3'
-import filterEb from '../../public/scores/filter/filter-Eb.musicxml'
-import filterBb from '../../public/scores/filter/filter-Bb.musicxml'
-import filterC from '../../public/scores/filter/filter-C.musicxml'
 import filterEbPdf from '../../public/scores/filter/filter-Eb.pdf'
 import filterBbPdf from '../../public/scores/filter/filter-Bb.pdf'
 import filterCPdf from '../../public/scores/filter/filter-C.pdf'
 import possessionTrack from '../../public/scores/possession/possession.mp3'
-import possessionC from '../../public/scores/possession/possession-C.musicxml'
 import possessionCPdf from '../../public/scores/possession/possession-C.pdf'
 import memeThemes1Track from '../../public/scores/meme-themes-1/meme-themes-1.mp3'
-import memeThemes1Score from '../../public/scores/meme-themes-1/meme-themes-1-score.musicxml'
 import memeThemes1ScorePdf from '../../public/scores/meme-themes-1/meme-themes-1-score.pdf'
 import thereWillNeverEbPdf from '../../public/scores/there-will-never/there-will-never-be-another-you-Eb.pdf'
 import thereWillNeverBbPdf from '../../public/scores/there-will-never/there-will-never-be-another-you-Bb.pdf'
@@ -89,189 +64,185 @@ import offTheWallEbPdf from '../../public/scores/off-the-wall/off-the-wall-Eb.pd
 import offTheWallCPdf from '../../public/scores/off-the-wall/off-the-wall-C.pdf'
 import offTheWallBbPdf from '../../public/scores/off-the-wall/off-the-wall-Bb.pdf'
 import offTheWallMixedPdf from '../../public/scores/off-the-wall/off-the-wall-mixed.pdf'
+import nineToFiveEbPdf from '../../public/scores/nine-to-five/nine-to-five-Eb.pdf'
+import nineToFiveCPdf from '../../public/scores/nine-to-five/nine-to-five-C.pdf'
+import nineToFiveBbPdf from '../../public/scores/nine-to-five/nine-to-five-Bb.pdf'
+import nineToFiveTrack from '../../public/scores/nine-to-five/nine-to-five.mp3'
+import songForCheEbPdf from '../../public/scores/song-for-che/song-for-che-Eb.pdf'
+import songForCheCPdf from '../../public/scores/song-for-che/song-for-che-C.pdf'
+import songForCheBbPdf from '../../public/scores/song-for-che/song-for-che-Bb.pdf'
+import songForCheTrack from '../../public/scores/song-for-che/song-for-che.mp3'
+
 
 const legalIllegalScores = [{
-    score: legalIllegalEb,
+    pdf: legalIllegalEbPdf,
     name: 'Eb',
   }, {
-    score: legalIllegalBb,
+    pdf: legalIllegalBbPdf,
     name: 'Bb',
   }, {
-    score: legalIllegalC,
+    pdf: legalIllegalCPdf,
     name: 'C',
 }]
 
 const iNeedYouScores = [{
-    score: iNeedYouEb,
     pdf: iNeedYouEbPdf,
     name: 'Eb',
   }, {
-    score: iNeedYouBb,
     pdf: iNeedYouBbPdf,
     name: 'Bb',
   }, {
-    score: iNeedYouC,
     pdf: iNeedYouCPdf,
     name: 'C',
 }]
 
 const eyeOfTheTigerScores = [{
-    score: eyeOfTheTigerEb,
     pdf: eyeOfTheTigerEbPdf,
     name: 'Eb',
   }, {
-    score: eyeOfTheTigerBb,
     pdf: eyeOfTheTigerBbPdf,
     name: 'Bb',
   }, {
-    score: eyeOfTheTigerC,
     pdf: eyeOfTheTigerCPdf,
     name: 'C',
 }]
 
 const madWorldScores = [{
-    score: madWorldEb,
     pdf: madWorldEbPdf,
     name: 'Eb',
   }, {
-    score: madWorldBb,
     pdf: madWorldBbPdf,
     name: 'Bb',
   }, {
-    score: madWorldC,
     pdf: madWorldCPdf,
     name: 'C',
 }]
 
 const pinkPantherScores = [{
-    score: pinkPantherEb,
     pdf: pinkPantherEbPdf,
     name: 'Eb',
   }, {
-    score: pinkPantherBb,
     pdf: pinkPantherBbPdf,
     name: 'Bb',
   }, {
-    score: pinkPantherC,
     pdf: pinkPantherCPdf,
     name: 'C',
 }]
 
 const happierScores = [{
-    score: happierEb,
     pdf: happierEbPdf,
     name: 'Eb',
   }, {
-    score: happierBb,
     pdf: happierBbPdf,
     name: 'Bb',
   }, {
-    score: happierC,
     pdf: happierCPdf,
     name: 'C',
 }]
 
 const filterScores = [{
-    score: filterEb,
     pdf: filterEbPdf,
     name: 'Eb',
   }, {
-    score: filterBb,
     pdf: filterBbPdf,
     name: 'Bb',
   }, {
-    score: filterC,
     pdf: filterCPdf,
     name: 'C',
 }]
 
 const godsPlanScores = [{
-    score: godsPlanEb,
     pdf: godsPlanEbPdf,
     name: 'Eb',
   }, {
-    score: godsPlanBb,
     pdf: godsPlanBbPdf,
     name: 'Bb',
   }, {
-    score: godsPlanC,
     pdf: godsPlanCPdf,
     name: 'C',
 }]
 
 const odeToJoyScores = [{
-    score: odeToJoyEb,
     pdf: odeToJoyEbPdf,
     name: 'Eb',
   }, {
-    score: odeToJoyBb,
     pdf: odeToJoyBbPdf,
     name: 'Bb',
   }, {
-    score: odeToJoyC,
     pdf: odeToJoyCPdf,
     name: 'C',
 }]
 
 const possessionScores = [{
-    score: possessionC,
     pdf: possessionCPdf,
     name: 'C'
 }]
 
 const memeThemes1Scores = [{
-    score: memeThemes1Score,
     pdf: memeThemes1ScorePdf,
     name: 'Score'
 }]
 
 const thereWillNeverScores = [{
-    score: null,
     pdf: thereWillNeverEbPdf,
     name: 'Eb'
   }, {
-    score: null,
     pdf: thereWillNeverBbPdf,
     name: 'Bb'
   }, {
-    score: null,
     pdf: thereWillNeverCPdf,
     name: 'C'
 }]
 
 const iWishIKnewScores = [{
-    score: null,
     pdf: iWishIKnewScorePdf,
     name: 'Score'
   }]
 
 const bagsAndTraneScores = [{
-    score: null,
     pdf: bagsAndTraneCPdf,
     name: 'C'
   },{
-    score: null,
     pdf: bagsAndTraneEbPdf,
     name: 'Eb'
   }]
 
   const offTheWallScores = [{
-    score: null,
     pdf: offTheWallEbPdf,
     name: 'Eb'
   }, {
-    score: null,
     pdf: offTheWallBbPdf,
     name: 'Bb'
   }, {
-    score: null,
     pdf: offTheWallCPdf,
     name: 'C'
-  },{
-  score: null,
-  pdf: offTheWallMixedPdf,
-  name: 'mixed'
+  }, {
+    pdf: offTheWallMixedPdf,
+    name: 'mixed'
 }]
+
+const nineToFiveScores = [{
+  pdf: nineToFiveEbPdf,
+  name: 'Eb'
+}, {
+  pdf: nineToFiveBbPdf,
+  name: 'Bb'
+}, {
+  pdf: nineToFiveCPdf,
+  name: 'C'
+}]
+
+const songForCheScores = [{
+  pdf: songForCheEbPdf,
+  name: 'Eb'
+}, {
+  pdf: songForCheBbPdf,
+  name: 'Bb'
+}, {
+  pdf: songForCheCPdf,
+  name: 'C'
+}]
+
 
 const Simple = cssTransition({
   enter: 'enter',
@@ -292,6 +263,9 @@ let scores = [{title: "Meme Themes 1", scores: memeThemes1Scores, track: memeThe
               {title: "I Wish I Knew How It Would Feel To Be Free ", scores: iWishIKnewScores, track: null, songId:"i-wish-i-knew", path:"/i-wish-i-knew"},
               {title: "Bags And Trane ", scores: bagsAndTraneScores, track: null, songId:"bags-and-trane", path:"/bags-and-trane"},
               {title: "Off The Wall ", scores: offTheWallScores, track: null, songId:"off-the-wall", path:"off-the-wall"},
+              {title: "Nine To Five ", scores: nineToFiveScores, track: nineToFiveTrack, songId:"off-the-wall", path:"off-the-wall"},
+              {title: "Legal Illegal ", scores: legalIllegalScores, track: legalIllegalTrack, songId:"legal-illegal", path:"legal-illegal"},
+              {title: "Song For Che ", scores: songForCheScores, track: songForCheTrack, songId:"song-for-che", path:"song-for-che"},
             ]
 
 class App extends Component {
